2a9c5bf7e3a1b299acb614b7c4c1bed4901cbbd4
jshh21 <48573515+jshh21@users.noreply.github.com>
2021-08-06 20:17:51 -0400

    Add files via upload

---
+++ arch.js
@@ -0,0 +1,8 @@
+let email = prompt("Inserte su email para un cupon de 15% en su proxima compra!");
+
+if (email > 100){
+    let cupon = .15 * "send to email";
+}
+else{
+    alert("Su descuento estara disponible en su email, ¡Bienvenidos!")
+}
